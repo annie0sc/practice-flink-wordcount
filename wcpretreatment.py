d38bc18adf2d6b2a1dc83f1bd3bbffdf4d716b01
annie0sc <annie0sc@gmail.com>
2021-02-19 00:37:47 -0600

    Updating the file paths to include new dataset.

--- wcpretreatment.py
+++ wcpretreatment.py
@@ -5,8 +5,10 @@ def wcpretreatment(path):
     with open(path, 'r') as f:
         jd = json.loads(f.read())
     sb = ''
-    for d in jd['features']:
-        if not d['properties']['SW_DIRECT'] == None:
-            sb += d['properties']['COUNTY_NA'] + d['properties']['VILL_NAME'] + '\n'
-    with open('./tmpfile', 'w', encoding='utf-8') as f:
+    for d in jd['records']:
+         if not d['popData2019'] == None:
+            sb += d['popData2019']['cases_weekly'] + d['popData2019']['deaths_weekly'] + '\n'
+        # if not d['properties']['SW_DIRECT'] == None:
+        #     sb += d['properties']['COUNTY_NA'] + d['properties']['VILL_NAME'] + '\n'
+    with open('./input', 'w', encoding='utf-8') as f:
         f.write(sb)
